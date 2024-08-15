edc09a9b053117c69029a48a0742880c13e78233
Anuj <invincibleanuj1718@gmail.com>
2024-08-15 10:51:37 +0530

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function App() {
   return (
     <div>
-      <h1>comment analyzer</h1>
+      <h1 className="text-xl font-bold">comment analyzer</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
