be201fcbc040c5d7f4ba7d9d2e0d264c009ba8a2
Anuj <invincibleanuj1718@gmail.com>
2024-10-09 15:12:36 +0530

    Files updated

--- Back-end/Login.js
+++ Back-end/Login.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-const auth = require('./routers/auth.js');
-const comments = require('./routers/comments.js');
-const dotenv = require('dotenv');
-const port = 3001;
-
+const auth = require("./routers/auth.js");
+const comments = require("./routers/comments.js");
+const dotenv = require("dotenv");
+const port = 5000;
 dotenv.config();
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1/CommentSense');
-  console.log('Connected to MongoDB');
+  await mongoose.connect("mongodb://127.0.0.1/CommentSense");
+  console.log("Connected to MongoDB");
 }
 
 app.get("/", (req, res) => {
   res.send("hello world");
-})
+});
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}`);
@@ -26,5 +25,5 @@ app.listen(port, () => {
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/auth', auth);
-app.use('/comments',comments);
+app.use("/auth", auth);
+app.use("/comments", comments);
--- Back-end/routers/comments.js
+++ Back-end/routers/comments.js
@@ -1,91 +1,90 @@
-const express = require('express');
+const express = require("express");
 const Comments = require("../models/Comments.js");
 const fetchuser = require("../middlewares/fetchuser.js");
-const { body, validationResult } = require('express-validator');
-const UrlModel = require('../models/UrlModel.js');
-const { default: mongoose } = require('mongoose');
-const { default: axios } = require('axios');
-const User = require('../models/User.js');
+const { body, validationResult } = require("express-validator");
+const UrlModel = require("../models/UrlModel.js");
+const { default: mongoose } = require("mongoose");
+const { default: axios } = require("axios");
+const User = require("../models/User.js");
 const router = express.Router();
-const ObjectId = require('mongoose').Types.ObjectId;
+const ObjectId = require("mongoose").Types.ObjectId;
 
-// ROUTE 1: Get all the comments using GET "/comments/getuser" Login required
-router.get('/fetchallcomments', fetchuser, async (req, res) => {
+// const API_BASE_URL =
+//   import.meta.env.API_BASE_URL;
+router.get("/fetchallcomments", fetchuser, async (req, res) => {
   try {
     const comments = await Comments.find({ user: req.user.id });
     res.json(comments);
+  } catch (error) {
+    console.log(error);
   }
-  catch (e) {
-    res.status(500).send("Internal Server Error");
-  }
-})
+});
 
-
-router.post('/addcomments', fetchuser, async (req, res) => {
+router.post("/addcomments", fetchuser, async (req, res) => {
   try {
-    // Fetch comments from the API and include the URL in the request body
     let response;
     try {
-      response = await axios.post("http://127.0.0.1:5000/get_comments", req.body);
+      response = await axios.post("http://127.0.0/get_comments", req.body);
       console.log(response.data);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
       console.log("Error", error.message);
     }
 
-    // Find the user by user ID
     const user = await User.findById(req.user.id);
 
-
     let urlDocument;
-    // Check if the URL is already present in the user's URLs
-    const existingUrl = await Promise.all(user.urls.map(async (urlId) => {
-      try {
-        // Find the URL document by ID
-        urlDocument = await UrlModel.findById(urlId);
-        if (urlDocument && urlDocument.url === req.body.videoId) {
-          // URL found and URL matches, return the document
-          return urlDocument;
+    const existingUrl = await Promise.all(
+      user.urls.map(async (urlId) => {
+        try {
+          urlDocument = await UrlModel.findById(urlId);
+          if (urlDocument && urlDocument.url === req.body.videoId) {
+            // URL found and URL matches, return the document
+            return urlDocument;
+          }
+          // URL not found or URL does not match, return null
+          return null;
+        } catch (error) {
+          console.error("Error while finding URL:", error);
+          return null;
         }
-        // URL not found or URL does not match, return null
-        return null;
-      } catch (error) {
-        console.error('Error while finding URL:', error);
-        return null;
-      }
-    }));
+      })
+    );
 
     // Filter out the null values, keeping only the existing URL documents
-    const filteredExistingUrls = existingUrl.filter(url => url);
+    const filteredExistingUrls = existingUrl.filter((url) => url);
 
     // Now, filteredExistingUrls contains only the URL documents that match the target URL
     if (filteredExistingUrls.length > 0) {
       // URLs found, do something with the array of matching URL documents
     } else {
       // No matching URLs found
-      console.log('No matching URLs found.');
+      console.log("No matching URLs found.");
     }
 
     if (filteredExistingUrls.length > 0) {
       // Delete existing comments in the Comments model
-      const deleteCommentPromises = filteredExistingUrls.map(async (existingUrl) => {
-        await Comments.deleteMany({ _id: { $in: existingUrl.comments } });
-      });
+      const deleteCommentPromises = filteredExistingUrls.map(
+        async (existingUrl) => {
+          await Comments.deleteMany({ _id: { $in: existingUrl.comments } });
+        }
+      );
 
       // Wait for all deletions to finish
       await Promise.all(deleteCommentPromises);
 
       // Create new comments and save them to the Comments model
-      const savedComments = await Promise.all(response.data.map(async (commentData) => {
-        commentData['user'] = req.user.id;
-        const newComment = new Comments(commentData);
-        return await newComment.save();
-      }));
+      const savedComments = await Promise.all(
+        response.data.map(async (commentData) => {
+          commentData["user"] = req.user.id;
+          const newComment = new Comments(commentData);
+          return await newComment.save();
+        })
+      );
 
       // Update the comments array in the existing URL documents
       const updatePromises = filteredExistingUrls.map(async (existingUrl) => {
-        existingUrl.comments = savedComments.map(comment => comment._id);
+        existingUrl.comments = savedComments.map((comment) => comment._id);
         return existingUrl.save();
       });
 
@@ -93,27 +92,26 @@ router.post('/addcomments', fetchuser, async (req, res) => {
       await Promise.all(updatePromises);
 
       // Update the comments array in the UrlModel documents
-      const urlModelUpdatePromises = filteredExistingUrls.map(async (existingUrl) => {
-        const urlModel = await UrlModel.findOneAndUpdate(
-          { _id: existingUrl._id },
-          { comments: existingUrl.comments },
-          { new: true }
-        );
-        return urlModel;
-      });
+      const urlModelUpdatePromises = filteredExistingUrls.map(
+        async (existingUrl) => {
+          const urlModel = await UrlModel.findOneAndUpdate(
+            { _id: existingUrl._id },
+            { comments: existingUrl.comments },
+            { new: true }
+          );
+          return urlModel;
+        }
+      );
 
       // Wait for all UrlModel updates to finish
       const updatedUrlModels = await Promise.all(urlModelUpdatePromises);
 
       res.send(response?.data);
-    }
-
-
-    else {
+    } else {
       // URL does not exist for the user, add new URL
       const savedComments = await Promise.all(
         response.data.map(async (comment) => {
-          comment['user'] = req.user.id;
+          comment["user"] = req.user.id;
           const newComment = new Comments(comment);
           return await newComment.save();
         })
@@ -122,7 +120,7 @@ router.post('/addcomments', fetchuser, async (req, res) => {
 
       const newUrl = new UrlModel({
         url: req.body.videoId,
-        comments: savedComments.map(comment => comment._id)
+        comments: savedComments.map((comment) => comment._id),
       });
 
       await newUrl.save();
@@ -133,12 +131,9 @@ router.post('/addcomments', fetchuser, async (req, res) => {
       res.send(response.data);
     }
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Internal Server Error');
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
   }
-
 });
-
-
 
 module.exports = router;
